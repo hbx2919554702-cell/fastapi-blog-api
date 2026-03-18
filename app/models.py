0dae0e1def3d23ee228e444ff0933176b5ffe55b
hbx666 <hbx2919554702@gmail.com>
2026-03-19 01:22:13 +0800

    将create_at和updata_at改为可为空

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ __tablename__ = 'article'
 class DBArticle(Base):
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-    __tablename__='article'
     id=Column(Integer,primary_key=True,index=True)
     title=Column(String(50))
     content=Column(String(200))
