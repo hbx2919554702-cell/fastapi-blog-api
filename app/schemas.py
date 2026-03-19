7a00552c2c0bc37586c8940005e42b2ee4ab1ace
hbx666 <hbx2919554702@gmail.com>
2026-03-20 01:34:19 +0800

    增加了Token和用户

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from typing import Optional
-
 from pydantic import BaseModel,Field
+
+#######################文章#####################
 # 增
 class ArticleCreate(BaseModel):
     title: str=Field(min_length=2,max_length=15,description="文章名要求2到15个字符")
@@ -26,3 +27,26 @@ class ArticleDetail(ArticleCreate):
 # 更新
 class ArticleUpdate(ArticleCreate):
     pass
+
+
+
+#################### 用户类######################
+#创建用户
+class UserCreate(BaseModel):
+    username: str=Field(min_length=2,max_length=50,description="用户名要求3到50个字符")
+    password: str=Field(max_length=6,description="密码至少6位")
+    email: Optional[str]=None
+
+# 返回
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: Optional[str]=None
+    class Config:
+        from_attributes = True
+
+# 返回Token
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    message: str="登陆成功"
