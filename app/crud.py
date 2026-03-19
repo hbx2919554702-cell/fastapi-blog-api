6154bc766f91a3b88c18a0b523887d855e1af31b
hbx666 <hbx2919554702@gmail.com>
2026-03-20 01:33:36 +0800

    增加用户的注册，登陆，搜索功能

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from sqlalchemy.orm import  Session
-from .models import DBArticle
-from .schemas import ArticleCreate, ArticleUpdate
-
+from .models import DBArticle,DBUser
+from .schemas import ArticleCreate, ArticleUpdate, UserCreate
+from .core.security import  get_password_hash
 
 # 根据id查询
 def get_article_id(db:Session,article_id:int):
@@ -46,3 +46,25 @@ def update_article(db:Session,article_id:int,article:ArticleUpdate):
         db.refresh(db_article)
         return db_article
     return  None
+
+
+####################用户##########################
+# 创建用户
+def create_user(db:Session,user:UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user=DBUser(username=user.username,email=user.email,hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# 用户模糊搜索
+def get_user_by_username(db:Session,skip:int=0,limit:int=10,keyword:str=None):
+    get_user_skip=db.query(DBUser)
+    if keyword:
+        get_user_skip=db.query(DBUser).filter(DBUser.username.ilike(f"%{keyword}%"))
+    return get_user_skip.offset(skip).limit(limit).all()
+
+# 登录或注册时是否有同名
+def get_users(db:Session,username:str):
+    return db.query(DBUser).filter(DBUser.username==username).first()
