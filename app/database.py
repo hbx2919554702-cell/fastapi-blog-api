c9909cb67e4b675059bc2efde83e12f189a2e47a
hbx666 <hbx2919554702@gmail.com>
2026-03-20 01:31:29 +0800

    让数据库自动命名

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import os
 from sqlalchemy import create_engine,Column,Integer,String
 from sqlalchemy.orm import sessionmaker,declarative_base
-
+from sqlalchemy import MetaData
 
 # 1. 配置数据库地址：这就相当于你的 SQL Server 连接字符串
 # sqlite:///./blog.db 意思是：在当前目录下，给我建一个名叫 blog.db 的文件当数据库
@@ -19,8 +19,18 @@ engine=create_engine(
 # 关闭自动确认和自动更新，绑定engine引擎
 SessionLocal=sessionmaker(bind=engine,autocommit=False,autoflush=False)
 
+# 数据库自动命名规则
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+# 带命名规则的 metadat
+metadata = MetaData(naming_convention=NAMING_CONVENTION)
 # 创建基础类
-Base=declarative_base()
+Base=declarative_base(metadata=metadata)
 
 # 定义一个获取数据库连接的生成器，这是 Depends 的核心
 def get_db():
