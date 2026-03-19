77a2c0262d3062c14845e2de299ed117f319f1ae
hbx666 <hbx2919554702@gmail.com>
2026-03-19 15:45:34 +0800

    替换了变量名，将put的自定义响应更改为AricleUppdate

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from typing import List, Optional
 from fastapi.responses  import RedirectResponse
 
-from .schemas import ArticleCreate,ArticleResponse,ArticleDetail
+from .schemas import ArticleCreate, ArticleResponse, ArticleDetail, ArticleUpdate
 from .database import engine,Base,get_db
 from . import crud
 
@@ -47,10 +47,10 @@ def read_articles(
 # 根据id读数据
 @app.get('/article/{article_id}',response_model=ArticleDetail)
 def read_article(article_id:int,db:Session = Depends(get_db)):
-    article_id=crud.get_article_id(db=db,article_id=article_id)
-    if article_id is None:
+    db_article_id=crud.get_article_id(db=db,article_id=article_id)
+    if db_article_id is None:
         raise HTTPException(status_code=404,detail='文章不存在')
-    return article_id
+    return db_article_id
 
 # 删除
 @app.delete('/article/{article_id}')
@@ -61,12 +61,12 @@ def delete_article(article_id:int,db:Session = Depends(get_db)):
     return {"message:",f"ID为{article_id}的文章删除成功"}
 
 # 更新
-@app.put('/article/{article_id}',response_model=ArticleDetail)
-def update_article(article_id:int,article:ArticleCreate,db:Session = Depends(get_db)):
-    update_article=crud.update_article(db=db,article_id=article_id,article=article)
-    if update_article is None:
+@app.put('/article/{article_id}',response_model=ArticleUpdate)
+def update_article(article_id:int,article:ArticleUpdate,db:Session = Depends(get_db)):
+    db_update_article=crud.update_article(db=db,article_id=article_id,article=article)
+    if db_update_article is None:
         raise HTTPException(status_code=404,detail="文章不存在")
-    return update_article
+    return db_update_article
 
 
 
