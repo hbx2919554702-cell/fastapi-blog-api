6d8e324fbc4fa7328ad8a69a0583a63b47659017
hbx666 <hbx2919554702@gmail.com>
2026-03-21 00:39:49 +0800

    DBuser表加入了昵称，性别，个人简介

--- app/models/users.py
+++ app/models/users.py
@@ -8,4 +8,7 @@ class DBUser(Base):
     hashed_password=Column(String(255),nullable=False,comment="用户密码")
     username=Column(String(50),unique=True,index=True,nullable=False,comment="用户名")
     email=Column(String(255),nullable=True,comment="电子邮箱(选填)")
+    nickname = Column(String(50), nullable=True,comment="昵称")
+    gender = Column(String(10), nullable=True, comment="性别")
+    bio = Column(String(255), nullable=True, default="这个人很懒，什么都没有留下",comment="简介")
     articles=relationship("DBArticle", back_populates="owner")
