911143a5f7bf45063f05d87fb855f8bc7f79641d
hbx666 <hbx2919554702@gmail.com>
2026-03-21 00:40:22 +0800

    修改了变量名错误的bug

--- app/crud/users.py
+++ app/crud/users.py
@@ -8,7 +8,7 @@ from app.schemas.users import UserCreate
 # 创建用户
 def create_user(db:Session,user:UserCreate):
     hashed_password = get_password_hash(user.password)
-    db_user=DBUser(username=user.username,email=user.email,hashed_password=hashed_password)
+    db_user=DBUser(username=user.username,hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
