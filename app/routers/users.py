eef0b871d3e0d18ba2a6c09aadf76e66d816f30a
hbx666 <hbx2919554702@gmail.com>
2026-03-21 00:39:25 +0800

    更新了用户注册通用响应

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,19 +2,25 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, Query
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+
+from app.core.response import success_response
 from app.database import get_db
-from app.schemas.users import UserResponse, UserCreate, Token
+from app.schemas.users import UserResponse, UserCreate, Token, UserAuthResponse, UserInfoResponse
 from app.crud.users import get_users, create_user, get_user_by_username
 from app.core.security import verify_password,create_access_token
 router = APIRouter(prefix="/api/users", tags=["users"])
 
 # 用户注册
-@router.post("/register",response_model=UserResponse)
-def register_user(user:UserCreate,db:Session=Depends(get_db)):
+@router.post("/register")
+def register_user(user:UserCreate, db:Session=Depends(get_db)):
     db_register_user=get_users(db=db,username=user.username)
     if db_register_user:
         raise HTTPException(status_code=400,detail="已存在相同用户名")
-    return create_user(db=db,user=user)
+    user=create_user(db=db,user=user)
+    token = create_access_token(data={"sub": user.username})
+    response_data=UserAuthResponse(token=token,userInfo=UserInfoResponse.model_validate(user))
+    return success_response(message="注册成功",data=response_data)
+
 
 # 登录
 @router.post("/login",response_model=Token)
@@ -27,6 +33,7 @@ def login_user(form_data: OAuth2PasswordRequestForm = Depends(),db:Session=Depen
             "token_type": "bearer",
             "message":"登陆成功！"}
 
+# 查找用户
 @router.get('/Users',response_model=List[UserResponse])
 def read_users(
                limit:int=Query(10,ge=1,le=20,description="每页数量"),
