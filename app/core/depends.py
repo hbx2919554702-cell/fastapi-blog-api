0e4a43abf5c783e7a33c0c08bac136ae52bebbe3
hbx666 <hbx2919554702@gmail.com>
2026-03-21 17:45:59 +0800

    登录校验器依赖（token校验->查询用户是否存在）

---
+++ app/core/depends.py
@@ -0,0 +1,34 @@
+# 封装获取Token到查询用户的工具函数(登录校验依赖)
+from fastapi import Depends, HTTPException,status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt,JWTError
+from sqlalchemy.orm import Session
+
+from app.crud.users import get_users
+from app.database import get_db
+from app.core.config import SECRET_KEY,ALGORITHM
+
+oauth2_scheme=OAuth2PasswordBearer(tokenUrl="/api/users/login")
+def get_current_user(token: str = Depends(oauth2_scheme),
+                     db: Session=Depends(get_db)):
+    try:
+        payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="无效的凭证(Token里没有用户名)")
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token无效或已过期,请重新登录"
+        )
+    user=get_users(db=db,username=username)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token验证成功，但无法找到该用户(可能已经被删除)"
+    )
+    return user
+
+
