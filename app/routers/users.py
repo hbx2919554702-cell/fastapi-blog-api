41e8bceb3d2f716c25bd104a6add55d179e6464b
hbx666 <hbx2919554702@gmail.com>
2026-03-21 17:44:13 +0800

    添加了查询个人信息，更改个人信息，更改密码的路由

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,13 +1,17 @@
 from typing import List, Optional
+
 from fastapi import APIRouter, Depends, HTTPException, Query
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import null
 from sqlalchemy.orm import Session
-
 from app.core.response import success_response
 from app.database import get_db
-from app.schemas.users import UserResponse, UserCreate, Token, UserAuthResponse, UserInfoResponse
-from app.crud.users import get_users, create_user, get_user_by_username
+from app.models.users import DBUser
+from app.schemas.users import UserResponse, UserCreate, Token, UserAuthResponse, UserInfoResponse, UserUpdateRequest, \
+    UserPassword
+from app.crud.users import get_users, create_user, get_user_by_username, update_user, update_password
 from app.core.security import verify_password,create_access_token
+from app.core.depends import get_current_user
 router = APIRouter(prefix="/api/users", tags=["users"])
 
 # 用户注册
@@ -28,6 +32,7 @@ def login_user(form_data: OAuth2PasswordRequestForm = Depends(),db:Session=Depen
     user=get_users(db=db,username=form_data.username)
     if not user or not verify_password(form_data.password,user.hashed_password):
         raise HTTPException(status_code=401,detail="用户密码错误")
+    # 登录以后拿到一个新token
     access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token,
             "token_type": "bearer",
@@ -46,3 +51,26 @@ def read_users(
     if user is None:
         raise HTTPException(status_code=404,detail="作者不存在")
     return user
+
+
+# 获取个人信息
+@router.get('/infe',response_model=List[UserInfoResponse])
+def get_user_infe(current_user: DBUser=Depends(get_current_user)):
+    user_info=UserInfoResponse.model_validate(current_user)
+    return success_response(message="获取用户信息成功！",data=user_info)
+
+# 更改个人信息
+@router.put('/update',response_model=List[UserInfoResponse])
+def update_user_infe(user_data:UserUpdateRequest,current_user: DBUser=Depends(get_current_user),db:Session=Depends(get_db)):
+    updated_user=update_user(username=current_user.username,update_user=user_data,db=db)
+    if updated_user is None:
+        raise HTTPException(status_code=404,detail="用户更新失败")
+    user_info=UserInfoResponse.model_validate(updated_user)
+    return success_response(message="个人信息更新成功",data=user_info)
+
+@router.put('/password',response_model=UserPassword)
+def update_user_password(password:UserPassword,current_user: DBUser=Depends(get_current_user),db:Session=Depends(get_db)):
+    updated_password=update_password(new_password=password.new_password,old_password=password.old_password,user=current_user,db=db)
+    if updated_password is None:
+        raise HTTPException(status_code=500,detail="密码更新失败")
+    return success_response(message="密码修改成功")
