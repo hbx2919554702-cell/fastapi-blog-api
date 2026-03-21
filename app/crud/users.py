0c862c94e0a07d96269d736ded71e7b2113a8712
hbx666 <hbx2919554702@gmail.com>
2026-03-21 17:44:44 +0800

    添加了更改个人信息，更改密码的方法

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,9 +1,10 @@
 ####################用户##########################
-# 创建用户
 from sqlalchemy.orm import Session
+from app.core import security
 from app.core.security import get_password_hash
 from app.models.users import DBUser
-from app.schemas.users import UserCreate
+from app.schemas.users import UserCreate, UserUpdateRequest
+from sqlalchemy import update
 
 # 创建用户
 def create_user(db:Session,user:UserCreate):
@@ -24,3 +25,30 @@ def get_user_by_username(db:Session,skip:int=0,limit:int=10,keyword:str=None):
 # 登录或注册时是否有同名
 def get_users(db:Session,username:str):
     return db.query(DBUser).filter(DBUser.username==username).first()
+
+
+# 修改个人信息
+def update_user(db:Session,username:str,update_user=UserUpdateRequest):
+    db_user=update_user.model_dump(exclude_unset=True,exclude_none=True)
+    if not db_user:
+        return get_users(db,username)
+    query=update(DBUser).where(DBUser.username==username).values(**db_user)
+    result=db.execute(query)
+    db.commit()
+
+    if result.rowcount==0:
+        return None
+    return get_users(db,username)
+
+# 修改密码
+def update_password(db:Session,user:DBUser,old_password:str,new_password:str):
+    if not security.verify_password(old_password,user.hashed_password):
+        return False
+    hash_new_password=security.get_password_hash(new_password)
+    user.hashed_password=hash_new_password
+    # 更新:由SQLALchemy真正接管这个User对象，确保可以commit
+    # 规避 session过期或关闭导致的不能提交的问题
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return True
