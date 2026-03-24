7cbce738960ed7df6473b408d7ead75bac834c34
hbx666 <hbx2919554702@gmail.com>
2026-03-25 00:53:44 +0800

    “停止追踪config.py并将其踢出仓库，并加入security.py的追踪”

---
+++ app/core/security.py
@@ -0,0 +1,29 @@
+from datetime import datetime,timedelta
+from typing import Optional
+from jose import jwt
+from passlib.context import CryptContext
+from .config import SECRET_KEY,ALGORITHM,ACCESS_TOKEN_EXPIRE_MINUTES
+
+# 创建密码上下文
+pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# 登陆时，验证密码
+def verify_password(plain_password:str,hashed_password:str)->bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+# 注册时，加密明文密码
+def get_password_hash(password:str) ->str:
+    return pwd_context.hash(password)
+
+# 创建JWT Token，根据用户ID生成一串Token
+def create_access_token(data:dict,expires_delta:Optional[timedelta]=None)->str:
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
