0170caa5893dd7987dd2c357c4d75a05b15d34b1
hbx666 <hbx2919554702@gmail.com>
2026-03-30 20:28:39 +0800

    修改了文章内容的为Text

--- app/models/articles.py
+++ app/models/articles.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import  DateTime, func, Integer, String, ForeignKey, Index
+from sqlalchemy import  DateTime, func, Integer, String, ForeignKey,Text
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from app.database import Base
 from typing import TYPE_CHECKING
@@ -15,7 +15,7 @@ class DBArticle(Base):
 
     id:Mapped[int]=mapped_column(Integer,primary_key=True,index=True,autoincrement=True,comment="文章编号")
     title:Mapped[str]=mapped_column(String(50),nullable=False,comment="标题")
-    content:Mapped[str]=mapped_column(String(255),comment="内容")
+    content:Mapped[str]=mapped_column(Text,comment="内容")
     created_at:Mapped[datetime]=mapped_column(DateTime, default=func.now(), comment="发布时间")
     updated_at:Mapped[datetime]=mapped_column(DateTime, default=func.now(), onupdate=func.now(), comment="更新时间")
     author_id:Mapped[int]=mapped_column(Integer, ForeignKey("user.id"),comment="作者")
