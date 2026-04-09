3566e79e871bb1dbefed0bc0fd4b1174af6ad5d1
hbx666 <hbx2919554702@gmail.com>
2026-04-09 19:20:40 +0800

    将返回结果提交到日志

--- app/core/cache_redis.py
+++ app/core/cache_redis.py
@@ -2,6 +2,7 @@ import json
 from typing import Any
 import redis.asyncio as redis
 from app.core.config import settings
+from app.core.logger import logger
 
 redis_client=redis.Redis(host=settings.REDIS_HOST,
             port=settings.REDIS_PORT,
@@ -13,7 +14,7 @@ async  def get_cache(key:str):
     try:
         return await redis_client.get(key)
     except Exception as e:
-        print(f"获取缓存{e}失败")
+        logger.error(f"获取缓存{e}失败")
         return None
 
 # 读字典或列表
@@ -23,7 +24,7 @@ async def get_json_cache(key:str):
         if date:
             return json.loads(date) #序列化
     except Exception as e:
-        print(f"获取JSON缓存{e}失败")
+        logger.error(f"获取JSON缓存{e}失败")
         return None
 
 # 设置缓存
@@ -34,7 +35,7 @@ async def set_cache(key:str,value=Any,expire:int=3600):
         await redis_client.setex(key,expire,value)
         return True
     except Exception as e:
-        print(f"设置缓存{e}失败")
+        logger.error(f"设置缓存{e}失败")
         return False
 
 # 清除缓存
@@ -47,5 +48,5 @@ async def delete_cached_articles(pattern:str):
                 await redis_client.delete(*key)
             return True
     except Exception as e:
-        print(f"批量删除缓存失败:{e}")
+        logger.error(f"批量删除缓存失败:{e}")
         return False
